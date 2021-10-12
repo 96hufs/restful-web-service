9823924019d2508ad0734ab69e815b48f958cc4b
96hufs <96hufs@gmail.com>
2021-10-12 23:33:48 +0900

    locale 처리 구현

--- src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.example.restfulwebservice.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController
 {
+    @Autowired
+    private MessageSource messageSource;
+
     //Get
     // /hello-world (endpoint)
     @GetMapping(path = "/hello-world")
@@ -25,6 +33,13 @@ public class HelloWorldController
     public HelloWorldBean helloWorldBean(@PathVariable String name)
     {
         return new HelloWorldBean( String.format("Hello World, %s", name) );
+    }
+
+    @GetMapping(path="/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) String strLocale) {
+        Locale locale = Locale.forLanguageTag(strLocale);
+        String message = messageSource.getMessage("greeting.message", null, locale);
+        return message;
     }
 
 }
