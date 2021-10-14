c588263d57ee867976e50e0e1eadd0bb5bf27672
96hufs <96hufs@gmail.com>
2021-10-15 00:12:46 +0900

    jpa

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into USER (ID, JOIN_DATE, NAME, PASSWORD, SSN) values(1, sysdate(), 'User1', 'test1111', '701010-1111111');
+insert into USER (ID, JOIN_DATE, NAME, PASSWORD, SSN) values(2, sysdate(), 'User2', 'test2222', '801010-2222222');
+insert into USER (ID, JOIN_DATE, NAME, PASSWORD, SSN) values(3, sysdate(), 'User3', 'test3333', '901010-3333333');
